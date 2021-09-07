ef1f66e584f84aecec5be4c994ea52a6c13cf0cf
PasinduManeka <manekaherat815@gmail.com>
2021-09-07 11:38:29 +0530

    CREATE: create the editing pallete

--- user/src/App.js
+++ user/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <h1>Article</h1>
+      <div className="container">
+        <CKEditor editor={ClassicEditor} />
+      </div>
     </div>
   );
 }

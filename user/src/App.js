9f937a98182150df0ee788ba1a936476e83c042b
PasinduManeka <manekaherat815@gmail.com>
2021-09-07 12:30:14 +0530

    CREATe: create to values display

--- user/src/App.js
+++ user/src/App.js
@@ -1,14 +1,23 @@
+import React, { useState } from "react";
 import "./App.css";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import ReactHtmlParser from "react-html-parser";
 
 function App() {
+  const [value, setValue] = useState("");
+
+  const handleOnChange = (e, editor) => {
+    const data = editor.getData();
+    setValue(data);
+  };
   return (
-    <div className="App container">
+    <div className="container">
       <h1>Article</h1>
       <div className="container">
-        <CKEditor editor={ClassicEditor} />
+        <CKEditor editor={ClassicEditor} onChange={handleOnChange} />
       </div>
+      <div>{ReactHtmlParser(value)}</div>
     </div>
   );
 }
